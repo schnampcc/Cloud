ddd29bef407a3688c9020ce4b075f4606523bb81
Mike Schnaser <mike.schnaser@pointclickcare.com>
2022-11-04 11:29:47 -0500

    Add project files

---
+++ Cloud/CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudApp.swift
+//  Cloud
+//
+//  Created by Mike Schnaser on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
